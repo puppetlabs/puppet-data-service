398a531c6eedd336c9eb2b944202ab3da601238a
Reid Vandewiele <reid@puppet.com>
2021-12-06 12:18:58 -0800

    Add yardoc documentation for abstract base class

--- app/lib/pds/data_adapter/base.rb
+++ app/lib/pds/data_adapter/base.rb
@@ -3,27 +3,42 @@ module PDS
     # This is an abstract base class documenting the required methods that
     # PDS::DataAdapter::Implementation classes must provide.
     class Base
-      def initialize
-        raise "Cannot initialize an abstract PDS::DataAdapter::Base class"
-      end
+      # Valid entity types:
+      #   - :users
+      #   - :nodes
+      #   - :hiera_data
 
-      def create(entity , filter: nil)
+      # @parama config [Hash] configuration needed to initialize the DataAdapter
+      def initialize(config)
         raise NotImplementedError
       end
 
-      def read(entity , filter: nil)
-        raise NotImplementedErrors
-      end
-
-      def upsert(entity , filter: nil)
+      # @param entity_type [Symbol] the entity type to operate on
+      # @param resources [Array] an array of resources to create
+      def create(entity_type, resources:)
         raise NotImplementedError
       end
 
-      def delete(entity , filter: nil)
+      # @param entity_type [Symbol] the entity type to operate on
+      # @param filter [Array] an array of filters to apply
+      # @return [Array] an array of resources
+      def read(entity_type, filter: nil)
         raise NotImplementedError
       end
 
-      # @return Symbol an identifier indicating the type of DataAdapter
+      # @param entity_type [Symbol] the entity type to operate on
+      # @param resources [Array] an array of resources to upsert
+      def upsert(entity_type, resources:)
+        raise NotImplementedError
+      end
+
+      # @param entity_type [Symbol] the entity type to operate on
+      # @return [Integer] the number of resources deleted
+      def delete(entity_type, filter: nil)
+        raise NotImplementedError
+      end
+
+      # @return [Symbol] an identifier indicating the type of DataAdapter
       def type
         raise NotImplementedError
       end
