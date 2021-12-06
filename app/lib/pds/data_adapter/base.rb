0ccf28e0be0fd2f0bdc8c4b7ba4ea072e37b92ed
Ed Mckain <mckain.default@gmail.com>
2021-12-06 18:54:08 +0100

    adds the basic structure for the PDS::DataAdapter abstract class

--- app/lib/pds/data_adapter/base.rb
+++ app/lib/pds/data_adapter/base.rb
@@ -1,9 +1,27 @@
 module PDS
   module DataAdapter
-
     # This is an abstract base class documenting the required methods that
     # PDS::DataAdapter::Implementation classes must provide.
     class Base
+      def initialize
+        raise "Cannot initialize an abstract PDS::DataAdapter::Base class"
+      end
+
+      def create
+        raise NotImplementedError
+      end
+
+      def read
+        raise NotImplementedErrors
+      end
+
+      def upsert
+        raise NotImplementedError
+      end
+
+      def delete
+        raise NotImplementedError
+      end
 
       # @return Symbol an identifier indicating the type of DataAdapter
       def type
