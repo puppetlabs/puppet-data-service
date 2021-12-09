7b6980f242107c81c1bf4ce9d231d9e1116c0fcb
Reid Vandewiele <reid@puppet.com>
2021-12-09 12:25:29 -0800

    Clean up database loading code
    This commit brings the database loading code back to a working state,
    while preserving the adapter separation.

--- app/lib/pds/data_adapter.rb
+++ app/lib/pds/data_adapter.rb
@@ -1,22 +1,21 @@
 
 module PDS
   module DataAdapter
-    def self.new(config)
-      raise "Invalid configuration!" unless config.is_a?(Hash)
-      # Determine and configure the appropriate implementation class
-      implementation = case config['adapter']
-                       when 'postgresql'
-                         load_postgresql_adapter
-                         PDS::DataAdapter::PostgreSQL
-                       when 'mock'
-                         load_mock_adapter
-                         PDS::DataAdapter::Mock
-                       else
-                         raise "Unsupported data adapter '#{config['adapter']}'!"
-                       end
+    def self.new(app)
+      database_config = app.settings.config.dig('database')
+      implementation = app.settings.config.dig('database', 'adapter')
 
       # Create and return a new instance of the implementation class
-      implementation.new(config)
+      case implementation
+      when 'postgresql'
+        load_postgresql_adapter(app)
+        PDS::DataAdapter::PostgreSQL.new(database_config)
+      when 'mock'
+        load_mock_adapter(app)
+        PDS::DataAdapter::Mock.new(database_config)
+      else
+        raise "Unsupported data adapter '#{implementation}'!"
+      end
     end
 
     # Used for raising exceptions
@@ -24,14 +23,22 @@ module PDS
 
     private
 
-    def self.load_mock_adapter
+    def self.load_mock_adapter(app)
       require_relative 'data_adapter/mock'
     end
 
-    def self.load_postgresql_adapter
+    def self.load_postgresql_adapter(app)
       require_relative 'data_adapter/postgresql'
+
+      # Set migrations_paths to a directory specific to the PostgreSQL adapter
       ActiveRecord::Migrator.migrations_paths = ['db/postgresql/migrate']
-      PDSApp.register Sinatra::ActiveRecordExtension
+
+      app.register Sinatra::ActiveRecordExtension
+
+      # Now that the ActiveRecordExtension has been registered, calling set on
+      # :database will have the effect of loading the config, and connecting to
+      # the database.
+      app.set :database, app.settings.config['database']
     end
   end
 end
--- app/pds_app.rb
+++ app/pds_app.rb
@@ -1,24 +1,27 @@
 require 'sinatra/base'
-require 'sinatra/config_file'
 require 'sinatra/custom_logger'
 require "sinatra/activerecord"
-require 'logger'
 require './lib/openapiing'
 require './lib/pds/data_adapter'
+require 'logger'
+require 'yaml'
 
 # only need to extend if you want special configuration!
 class PDSApp < OpenAPIing
-  register Sinatra::ConfigFile
-
   # Set required defaults, then load full config from file.
   # File values, if given, will replace defaults.
   set :logger, Logger.new(STDOUT)
   set :default_content_type, :json
-  set :database, { 'adapter' => 'unconfigured' }
 
   # Users should configure their app settings using a pds.yaml file in one of
   # these locations
-  config_file '/etc/puppetlabs/pds/pds.yaml', File.expand_path(File.join(__dir__, 'config', 'pds.yaml'))
+  searchpath = [
+    '/etc/puppetlabs/pds/pds.yaml',
+    File.expand_path(File.join(__dir__, 'config', 'pds.yaml')),
+  ]
+
+  configpath = searchpath.find { |f| File.exist?(f) }
+  set :config, configpath.nil? ? {} : YAML.load_file(configpath)
 
   self.configure do |config|
     config.api_version = '1.0.0'
@@ -28,7 +31,7 @@ end
 # Based on the user-supplied hash 'database', set :data_adapter to an
 # appropriate DataAdapter object. This is performed here so that it is
 # universally invoked when this file is required.
-PDSApp.set :data_adapter, PDS::DataAdapter.new(PDSApp.settings.database)
+PDSApp.set :data_adapter, PDS::DataAdapter.new(PDSApp)
 
 # include the helpers
 Dir["./helpers/*.rb"].each { |file|
