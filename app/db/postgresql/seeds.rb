4c3e9522232aac0135d99dd54a5296a719f803ae
Ed Mckain <mckain.default@gmail.com>
2021-12-15 19:29:32 +0100

    completes the db:seeds for User, Node and HieraData

--- app/db/postgresql/seeds.rb
+++ app/db/postgresql/seeds.rb
@@ -6,7 +6,34 @@ if App.environment == :development
 
   business_domain_name = 'acme.com'
   users = ['bob', 'alice', 'mary', 'jon', 'carlos']
-    
+  nodes = [
+    {
+      name: "ip-100.#{business_domain_name}",
+      code_environment: 'development',
+      classes: ['policy::base'],
+      trusted_data: ''
+    },
+    {
+      name: "ip-101.#{business_domain_name}",
+      code_environment: 'development',
+      classes: ['policy::base'],
+      trusted_data: ''
+    },
+    {
+      name: "ip-102.#{business_domain_name}",
+      code_environment: 'production',
+      classes: ['policy::base', 'policy::tier1'],
+      trusted_data: ''
+    },
+  ]
+
+  hiera_data = [
+    { level: 'common', key: 'pds::color', value: 'red' },
+    { level: 'priority', key: 'pds::color', value: 'blue' },
+    { level: 'common', key: 'pds::distance', value: '10k' },
+    { level: 'priority', key: 'pds::distance', value: '42k' },
+  ]
+
   users.each do |user|
     puts "Creating user #{user.capitalize} ... \n"
     PDS::DataAdapter::PostgreSQL::User.create!(
@@ -14,6 +41,16 @@ if App.environment == :development
       email: "#{user}@#{business_domain_name}",
       role: 'operator'
     )
+  end
+
+  nodes.each do |node_data|
+    puts "Creating node #{node_data[:name]} ... \n"
+    PDS::DataAdapter::PostgreSQL::Node.create!(node_data)
+  end
+
+  hiera_data.each do |data|
+    puts "Creating hiera data #{data[:level]}/#{data[:key]} ... \n"
+    PDS::DataAdapter::PostgreSQL::HieraData.create!(data)
   end
 else
   puts 'Error: db:seeds can only run in development' 
