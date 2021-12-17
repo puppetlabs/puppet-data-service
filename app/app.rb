89152d6f0d21c3948eb13b237fb07d8f7fbe9da1
Ed Mckain <mckain.default@gmail.com>
2021-12-17 18:39:26 +0100

    loads lib/ folder from 1 single centrla location app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,7 @@
+# Enable easy use of `require` for content in lib/
+libdir = File.expand_path(File.join(__dir__, 'lib'))
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+
 require 'sinatra/base'
 require 'sinatra/custom_logger'
 require 'openapiing'
