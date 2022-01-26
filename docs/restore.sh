d5b11f8b5502d9ffd9029c6ddd8161cffb37e1a6
Cody Herriges <193064+ody@users.noreply.github.com>
2022-01-26 17:01:22 +0000

    Remove accidental restore redirect

--- docs/restore.sh
+++ docs/restore.sh
@@ -10,7 +10,7 @@ CA=$(grep ca-file /etc/puppetlabs/pds/pds-client.yaml | awk '{print $2}')
 tar -xzf $1 -C $RESTORE_WORKING_DIR
 
 for i in users hiera-data nodes; do
-  curl -s -k --cacert $CA -X POST $URI/$i -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" --data-binary @$RESTORE_WORKING_DIR/$i.json > $BACKUP_WORKING_DIR/$i.json
+  curl -s -k --cacert $CA -X POST $URI/$i -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" --data-binary @$RESTORE_WORKING_DIR/$i.json
 done
 
 rm -rf $RESTORE_WORKING_DIR
