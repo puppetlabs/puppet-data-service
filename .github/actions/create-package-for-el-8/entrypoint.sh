54d430f5474f5fa0df0575675a82b6c8e85176ed
Reid Vandewiele <reid@puppet.com>
2022-02-05 11:36:26 -0800

    More trying to make EL 7 work

--- .github/actions/create-package-for-el-8/entrypoint.sh
+++ .github/actions/create-package-for-el-8/entrypoint.sh
@@ -5,37 +5,36 @@ set -e
 
 PE_VERSION=${1:-2021.4.0}
 PLATFORM=${PLATFORM:-el-8}
-WORKDIR=${GITHUB_WORKSPACE:-/workspace}
+GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-/workspace}
+SCRATCHDIR=${SCRATCHDIR:-/scratch}
 
-# Pre-requisites
+# Download PE and install required dependent packages
 
-mkdir -p "${WORKDIR}"
-curl -o "${WORKDIR}/pe.tar.gz" "https://s3.amazonaws.com/pe-builds/released/${PE_VERSION}/puppet-enterprise-${PE_VERSION}-${PLATFORM}-x86_64.tar.gz"
-
-mkdir -p "${WORKDIR}/pe"
-tar -C "${WORKDIR}/pe" -xzf "${WORKDIR}/pe.tar.gz" --strip-components 1
+mkdir -p "${SCRATCHDIR}/pe"
+curl -o "${SCRATCHDIR}/pe.tar.gz" "https://s3.amazonaws.com/pe-builds/released/${PE_VERSION}/puppet-enterprise-${PE_VERSION}-${PLATFORM}-x86_64.tar.gz"
+tar -C "${SCRATCHDIR}/pe" -xzf "${SCRATCHDIR}/pe.tar.gz" --strip-components 1
 
 cat > /etc/yum.repos.d/pe.repo <<EOF
 [pe]
 name=Puppet, Inc. PE Packages \$releasever - \$basearch
-baseurl=file://${WORKDIR}/pe/packages/${PLATFORM}-x86_64
+baseurl=file://${SCRATCHDIR}/pe/packages/${PLATFORM}-x86_64
 enabled=True
 gpgcheck=1
-gpgkey=file://${WORKDIR}/pe/packages/GPG-KEY-puppet
-        file://${WORKDIR}/pe/packages/GPG-KEY-puppet-2025-04-06
-proxy=
+gpgkey=file://${SCRATCHDIR}/pe/packages/GPG-KEY-puppet
+        file://${SCRATCHDIR}/pe/packages/GPG-KEY-puppet-2025-04-06
 EOF
-
-# Continue the process
 
 yum install -y puppet-agent pe-puppet-enterprise-release pe-postgresql11-devel
 
-git clone https://github.com/puppetlabs/puppet-data-service.git
-pushd puppet-data-service
+# Create the RPM
+
+# It is assumed/required that the puppet-data-service project is already
+# checked out in the GITHUB_WORKSPACE.
+mkdir -p "${GITHUB_WORKSPACE}"
+pushd "${GITHUB_WORKSPACE}"
   make clean
   make rpm
-  RPMFILE=$(ls *.rpm | head -1)
-  mv "${RPMFILE}" "${WORKDIR}"
+  RPMFILE=$(ls -t *.rpm | head -1)
 popd
 
 echo "::set-output name=filename::${RPMFILE}"
